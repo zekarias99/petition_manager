378104df630e8b0ccbfaa7b7872cc97d66e8ded7
Ben Woodall <mail@benwoodall.com>
2013-01-15 18:37:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetitionManager::Application.config.secret_token = '8877f8606661e544e26dc64e2f6f8dc8118614dde6e487e76122d8153ad8173d309d81ef3c9ebc45e9f71f89f6854633bf58e4b93b32d81c84e263b92353b656'
