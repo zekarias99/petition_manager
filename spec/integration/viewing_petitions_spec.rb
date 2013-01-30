80f1df19d7b72fd056fcf75dbe4aab37182f5599
Ben Woodall <mail@benwoodall.com>
2013-01-29 18:16:27 -0800

    wtf

--- spec/integration/viewing_petitions_spec.rb
+++ spec/integration/viewing_petitions_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 feature "Viewing petitions" do
 
   scenario "Listing all open petitions" do
-    project = FactoryGirl.build(:petition, {:id => 1, :approved => true})
+    project = FactoryGirl.build(:petition, :exception => 'waiver')
     visit '/petitions'
-    click_link '1'
+    click_link 'petition_path'
     page.current_url.should == petition_url(petition)
   end
 
