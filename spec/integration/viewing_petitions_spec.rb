2ace4c08c0b56beef0bf49e6b84c76fe098b8259
Ben Woodall <mail@benwoodall.com>
2013-01-29 20:26:57 -0800

    wtf

---
+++ spec/integration/viewing_petitions_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature "Viewing petitions" do
+
+  scenario "Listing all open petitions" do
+    project = FactoryGirl.create(:petition, :exception => 'waiver')
+    visit '/petitions'
+    page.should have_content("1")
+    page.current_url.should == petition_url(petition)
+  end
+
+end
