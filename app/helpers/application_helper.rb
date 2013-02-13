e91b9a88833aaa15da2a14f1d1d8d0baf73e7a2c
dp <pham.danny.t@gmail.com>
2013-02-12 20:11:19 -0800

    added programs table with title sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
-	def title(*parts)
-		unless parts.empty?
-			content_for :title do 
-				(parts << "Petition Manager").join(" - ")
-			end
-		end
-	end
+  def title(*parts)
+    unless parts.empty?
+	    content_for :title do 
+	      (parts << "Petition Manager").join(" - ")
+	    end
+	  end
+  end 
+  def sortable(column, title = nil)  
+    title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"  
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+programs = [ {:title => 'Business Management', :owner => 'Professor Business'},
+             {:title => 'Digital Media Arts', :owner => 'Professor Media'},
+             {:title => 'Paraprofessional Accounting', :owner => 'Professor Accounting'},
+             {:title => 'Marketing Management', :owner => 'Professor Marketing'},
+             {:title => 'Information Systems', :owner => 'Professor Information'},
+             {:title => 'Network Services & Computing Systems', :owner => 'Professor Networking'},
+             {:title => 'Sustainable Business Practices', :owner => 'Professor Sustainable'},
+             {:title => 'Business Technology Systems', :owner => 'Professor BTS'},
+           ]
+
+programs.each do |program|
+  Program.create!(program)
+end
