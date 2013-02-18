cba0b7762298cd39ec096fccbc9ab5a5781c4741
Ben Woodall <mail@benwoodall.com>
2013-02-18 09:10:48 -0800

    added devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PetitionManager::Application.routes.draw do
+  devise_for :users
+
   resources :programs, :courses, :petitions
   match 'programs/' => 'programs#index'
   match 'courses/' => 'courses#index'
