fdde120a5395210ead9b352a65612e7ee3c15c91
dp <pham.danny.t@gmail.com>
2013-03-07 19:08:53 -0800

    petitions index is temporary root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 PetitionManager::Application.routes.draw do
   resources :programs, :courses, :petitions
+
+  root :to => 'petitions#index'
+  
   match 'programs/' => 'programs#index'
   match 'courses/' => 'courses#index'
-
-  # root :to => 'welcome#index'
-
 end
