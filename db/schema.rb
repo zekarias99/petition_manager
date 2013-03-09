b2f4acba2c34bcb9c5fedabbb5c150aa15802602
Ben Woodall <mail@benwoodall.com>
2013-03-09 12:32:47 -0800

    updated petetion status

--- db/schema.rb
+++ db/schema.rb
@@ -22,14 +22,6 @@ ActiveRecord::Schema.define(:version => 20130308053702) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "faculties", :force => true do |t|
-    t.string   "username"
-    t.string   "facutly_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string   "email"
-  end
-
   create_table "petitions", :force => true do |t|
     t.string   "exception"
     t.string   "reason"
@@ -45,11 +37,23 @@ ActiveRecord::Schema.define(:version => 20130308053702) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "students", :force => true do |t|
-    t.string   "username"
-    t.string   "student_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "type"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
